4db0809f9c34d0ce6e09ef11c6a9040f97622bab
Sayyid Hani Abbas Ja'fari <k250085@nu.edu.pk>
2025-09-28 16:29:19 +0500

    Add files via upload

---
+++ pf task home 5 Q1.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+	
+	int age;
+	printf("Enter age:\n");
+	scanf("%d", &age);
+	
+	if (age<5) {
+		printf("Free!\n");
+		
+	}
+	
+	else if (age>65) {
+		printf("Discount!\n");
+	}
+	
+	else {
+		printf("Regular!\n");
+	}	
+	
+	return 0;
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
